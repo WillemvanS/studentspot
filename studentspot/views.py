426eaf29ed4dc4ed617683ea1382d4ceb62ac2dc
Willem van Santvoort <willemvsantvoort@gmail.com>
2018-01-10 01:36:48 +0100

    Started on the view of the calendar.

--- studentspot/views.py
+++ studentspot/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Day
 
 # Create your views here.
-
-#Renders a html page for show_calendar using the show_calendar.html template
+days_forward = 999900 #how many days the calendar should show
+#Renders a html page for show_calendar using the show_calendar.html template and the selected days
 def show_calendar(request):
-    return render(request, 'studentspot/show_calendar.html')
+    days = (Day.objects.filter(date__lte=(timezone.now() + timezone.timedelta(days=days_forward)))).filter(date__gte=timezone.now()).order_by('date')
+    return render(request, 'studentspot/show_calendar.html', {'days': days})
