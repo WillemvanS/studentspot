805b577dcf019c1cb6b49b370bb67f489d62c187
Willem van Santvoort <willemvsantvoort@gmail.com>
2018-01-11 23:48:25 +0100

    Vult dagen aan

--- studentspot/views.py
+++ studentspot/views.py
@@ -6,6 +6,9 @@ from .models import Day, House
 days_forward = 7 #how many days the calendar should show
 #Renders a html page for show_calendar using the show_calendar.html template and the selected days
 def show_calendar(request):
+    for x in range (0, days_forward):
+        if not Day.objects.filter(date=(timezone.now() + timezone.timedelta(days=x))).exists():
+            Day.objects.create(date=(timezone.now() + timezone.timedelta(days=x)))
     days = (Day.objects.filter(date__lte=(timezone.now() + timezone.timedelta(days=days_forward)))).filter(date__gte=timezone.now()).order_by('date')
     houses = (House.objects.filter(houseName="Group_9"))
     return render(request, 'studentspot/show_calendar.html', {'days': days, 'houses' : houses})
