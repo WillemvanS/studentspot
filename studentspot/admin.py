07090077396c5c0544c8ad593c921f32a23dd146
Willem van Santvoort <willemvsantvoort@gmail.com>
2018-01-11 16:28:16 +0100

    Calendar geeft nu status weer van bestaande bewoners

--- studentspot/admin.py
+++ studentspot/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Day
+from .models import Day, House
 
 admin.site.register(Day)
+admin.site.register(House)
--- studentspot/views.py
+++ studentspot/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Day
+from .models import Day, House
 
 # Create your views here.
 days_forward = 7 #how many days the calendar should show
 #Renders a html page for show_calendar using the show_calendar.html template and the selected days
 def show_calendar(request):
     days = (Day.objects.filter(date__lte=(timezone.now() + timezone.timedelta(days=days_forward)))).filter(date__gte=timezone.now()).order_by('date')
-    return render(request, 'studentspot/show_calendar.html', {'days': days})
+    houses = (House.objects.filter(houseName="Group_9"))
+    return render(request, 'studentspot/show_calendar.html', {'days': days, 'houses' : houses})
