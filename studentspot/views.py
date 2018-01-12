b5cea86583dc7b7cae4dedb0c20fa5d0a38e550d
Aarts <a.aarts@student.tue.nl>
2018-01-12 15:22:13 +0100

    mm

--- studentspot/views.py
+++ studentspot/views.py
@@ -14,7 +14,7 @@ def show_calendar(request):
     return render(request, 'studentspot/show_calendar.html', {'days': days, 'houses' : houses})
 
 def homepage(request):
-    return render(request, 'studentspot/homepagetest.html')
+    return render(request, 'studentspot/homepage.html')
 
 def loginpage(request):
     return render(request, 'studentspot/loginpage.html')
