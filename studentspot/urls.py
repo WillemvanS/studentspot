bc3261c9f4314b02207ce03a67feaa4157626c48
Aarts <a.aarts@student.tue.nl>
2018-01-12 15:58:43 +0100

    mm

--- studentspot/urls.py
+++ studentspot/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 #see views.py for what the urls refer to
 urlpatterns = [
-    url(r'^$', views.show_calendar, name='show_calendar'),
-    url(r'^homepage/$', views.homepage, name='homepage'),
+    url(r'^calendar/$', views.show_calendar, name='show_calendar'),
+    url(r'^$', views.homepage, name='homepage'),
      url(r'^accounts/login/$', views.loginpage, name='login'),
 ]
