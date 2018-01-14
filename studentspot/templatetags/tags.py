d0b23b4518ff6e6f578ea0c54be9b5bc98fc1555
Aarts <a.aarts@student.tue.nl>
2018-01-14 19:59:00 +0100

    m

--- studentspot/templatetags/tags.py
+++ studentspot/templatetags/tags.py
@@ -7,3 +7,6 @@ def check_group():
    my_group = Group.objects.get(name='Test Huis')
    if my_group in request.user.groups.all():
        return True
+
+def is_member(user):
+    return user.groups.filter(name='Test Huis').exists()
