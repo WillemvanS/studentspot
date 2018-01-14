1084d4cbf6f6a3ebfa1f443a88d42f73d84301ef
Aarts <a.aarts@student.tue.nl>
2018-01-14 21:16:27 +0100

    m

--- studentspot/views.py
+++ studentspot/views.py
@@ -17,6 +17,7 @@ def show_calendar(request):
     days = (Day.objects.filter(date__lte=(timezone.now() + timezone.timedelta(days=forward)))).filter(date__gte=timezone.now()).order_by('date')
     houses = (House.objects.filter(houseName="Group_9"))
     group = request.user.groups.all()[0]
+    allgroups = Group.objects.all()
     users = group.user_set.all()
     return render(request, 'studentspot/show_calendar.html', {'days': days, 'houses' : houses, 'group' : group, 'users' : users})
 
