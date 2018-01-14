665f30077cb7ed20d63c9431034e9fe4e7a5a9fa
Aarts <a.aarts@student.tue.nl>
2018-01-14 17:15:50 +0100

    m

--- studentspot/views.py
+++ studentspot/views.py
@@ -40,3 +40,6 @@ def add_to_group(request):
    my_group = Group.objects.get(name='Test Huis')
    my_group.user_set.add(request.user.id)
    return render(request, 'studentspot/homepage.html')
+
+ def is_member(user):
+    return user.groups.filter(name='Test Huis').exists()
