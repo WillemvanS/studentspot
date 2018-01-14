c6d2ea81292a617af05a26f05a0646fccb67a805
Aarts <a.aarts@student.tue.nl>
2018-01-14 18:45:58 +0100

    m

--- studentspot/templatetags/tags.py
+++ studentspot/templatetags/tags.py
@@ -2,7 +2,7 @@ from django import template
 
 register = template.Library()
 
-@register.group_tag
+@register.simple_tag
 def check_group():
    my_group = Group.objects.get(name='Test Huis')
    if my_group in request.user.groups.all():
